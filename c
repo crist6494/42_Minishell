80975cf92cfb9006dae72cabdd6de9c203520717
crist6494 <moralesrojascr@gmail.com.com>
2023-03-21 11:17:50 +0100

    VAR Expansions when use heredoc and fix a problem when put the same letter twice

---
+++ c
@@ -0,0 +1,3 @@
+/Users/cmorales/42/Minishell2.0
+/Users/cmorales/42/Minishell2.0
+pepe
--- src/redirections/heredoc.c
+++ src/redirections/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: cmorales <moralesrojascr@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 20:04:50 by cmorales          #+#    #+#             */
-/*   Updated: 2023/03/20 19:53:23 by cmorales         ###   ########.fr       */
+/*   Updated: 2023/03/21 11:15:03 by cmorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,78 @@
 
 static int	check_heredoc_del(t_ms *ms, t_token *token, char *user_input)
 {
+	int	tlen;
+	int	ulen;
+
 	(void)ms;
 	if (user_input == NULL)
 	{
 		return (1);
 	}
-	if (ft_strncmp(user_input, token->str, ft_strlen(token->str)) == 0)
+	ulen = ft_strlen(user_input);
+	tlen = ft_strlen(token->str);
+	if (ulen == tlen && ft_strncmp(user_input, token->str, tlen) == 0)
 		return (1);
 	return (0);
 }
+
+static char	*get_word(char *line, int i, int j)
+{
+	char	*word;
+	int		k;
+
+	word = malloc(sizeof(char) * (j - i + 1));
+	if (!word)
+		return (NULL);
+	k = 0;
+	while (i != j)
+	{
+		word[k] = line[i];
+		k++;
+		i++;
+	}
+	word[k] = '\0';
+	return (word);
+}
+
+static void	write_env_if_exists(t_ms *ms, char *var, int fd)
+{
+	char	*new_var;
+	int		j;
+
+	new_var = get_env_value(var, ms->env);
+	j = 0;
+	while (new_var[j])
+		write(fd, &new_var[j++], 1);
+	free(new_var);
+}
+
+static void	write_line_heredoc(t_ms *ms, char *line, int fd)
+{
+	int		i;
+	int		j;
+	char	*var;
+
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] != '$')
+			write(fd, &line[i++], 1);
+		else
+		{
+			j = ++i;
+			while (ft_isalnum(line[j]))
+				j++;
+			if (i == j)
+				return ;
+			var = get_word(line, i, j);
+			write_env_if_exists(ms, var, fd);
+			i = j;
+			free(var);
+		}
+	}
+}
+
 
 void	heredoc(t_ms *ms, t_token *token)
 {
@@ -37,7 +100,7 @@ void	heredoc(t_ms *ms, t_token *token)
 		line = readline("> ");
 		if (check_heredoc_del(ms, token, line) == 1)
 			break ;
-		ft_putstr_fd(line, fd[1]);
+		write_line_heredoc(ms, line, fd[1]);
 		ft_putstr_fd("\n", fd[1]);
 		free(line);
 	}
